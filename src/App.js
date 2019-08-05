a7f143b2c681a37ab766cf3a7850d4072db676ba
botelja <ivo.surjak@gmail.com>
2019-08-05 15:59:54 +0200

    update dependecies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import RandomColor from './components/RandomColor';
 import './App.css';
 
 function App() {
@@ -7,9 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RandomColor />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/RandomColor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RandomColor() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default RandomColor
