42c213650c4b31a1cef38bc79b9dbf9dd4dd07e8
botelja <ivo.surjak@gmail.com>
2019-08-06 11:49:34 +0200

    Make RandomColor comoponent

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import RandomColor from './components/RandomColor';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState([])
+  const [counter, setCounter] = useState(0)
+ 
+
+  useEffect(() => {
+
+    
+    const fetchData = async () => {
+      const result = await fetch('http://www.colr.org/json/color/random')
+      const data = await result.json()
+      setColor(data.new_color)
+      
+      console.log('Call fetch')
+    }
+
+    fetchData()
+    
+    
+  }, [counter])
+
+
+  const handleClick = () => {
+    setCounter(counter + 1)
+    console.log('Click')
+  }
+
+  
+ 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <RandomColor />
+        <RandomColor 
+          color={color}
+          handleClick={handleClick}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/components/RandomColor.js
+++ src/components/RandomColor.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-function RandomColor() {
+function RandomColor({color, handleClick}) {
+
+    console.log(color)
+
     return (
         <div>
-            
+           <p 
+            style={{color: `#${color}`}}
+            onClick={handleClick}
+            >
+                Hello World {color}
+            </p> 
         </div>
     )
 }
